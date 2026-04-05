78292012b1cc5874a1a5dae68b144471438496a1
mans00rahmed <mansoorahmedy2k@gmail.com>
2026-04-05 17:39:08 +0100

    chore: initial repository setup

---
+++ src/test/java/com/sigma/smarthome/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sigma.smarthome.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false"
+})
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
